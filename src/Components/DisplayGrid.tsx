6049ea9e79fab741539648f96fc816746923559f
Sashwin <sashwincareer@gmail.com>
2024-10-16 21:23:56 +0530

    Optimize commit 2 16/10

--- src/Components/DisplayGrid.tsx
+++ src/Components/DisplayGrid.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import Cell from "./Cell";
+import React, { lazy, Suspense } from "react";
 import Input from "./Input";
-import { Button, Grid } from "@mui/material";
+import { Button } from "@mui/material";
+
+const LazyGrid=lazy(()=>import("./Grid"))
 
 interface Display {
   handleOpen: (a: string) => void;
@@ -119,64 +120,9 @@ const DisplayGrid: React.FC<Display> = ({
               (Group of 3 Blocks)
             </Button>
           </div>
-
-          {grid.map((row: any, rowIndex: number) => (
-            <Grid item xs={12} key={rowIndex}>
-              <Grid container justifyContent="center">
-                {row.map((cell: any, colIndex: number) => {
-                  if (colIndex === 0 && rowIndex === 0) {
-                    return (
-                      <div
-                        key={colIndex}
-                        style={{
-                          border: "1px solid grey",
-                          color: "black",
-                          width: "57px",
-                        }}
-                      >
-                        {" "}
-                      </div>
-                    );
-                  } else if (colIndex === 0) {
-                    return (
-                      <Button
-                        key={colIndex}
-                        variant="outlined"
-                        sx={{ border: "1px solid grey", color: "black" }}
-                        onClick={() => handleChange(cell.id, rowIndex)} // Note: Adjusted for any errors in typing
-                      >
-                        {cell ? cell.id : ""}{" "}
-                        {/* Display the text property if the cell is not null */}
-                      </Button>
-                    );
-                  } else if (rowIndex === 0) {
-                    return (
-                      <Button
-                        key={colIndex}
-                        variant="outlined"
-                        sx={{ border: "1px solid grey", color: "black" }}
-                        onClick={() => handleChange(cell.id, colIndex)}
-                      >
-                        {cell ? cell.id : ""}{" "}
-                        {/* Display the text property if the cell is not null */}
-                      </Button>
-                    );
-                  } else {
-                    return (
-                      <Grid item key={colIndex}>
-                        <Cell
-                          rowIndex={rowIndex}
-                          colIndex={colIndex}
-                          cell={cell}
-                          moveBlock={moveBlock}
-                        />
-                      </Grid>
-                    );
-                  }
-                })}
-              </Grid>
-            </Grid>
-          ))}
+          <Suspense fallback={<h2 style={{color:"black"}}>Loading Grid...</h2>}>
+            <LazyGrid grid={grid} handleChange={handleChange} moveBlock={moveBlock} />
+          </Suspense>
         </div>
       )}
     </div>
---
+++ src/Components/Grid.tsx
@@ -0,0 +1,68 @@
+import Cell from "./Cell";
+import { Button, Grid } from "@mui/material";
+
+const GridComp:React.FC<{  grid: any,handleChange: (cell: string, index: number) => void, moveBlock: (id:string,toCol:number,toRow:number) => void;}>=({grid,handleChange,moveBlock})=>{
+    return(
+       <div>
+         {grid.map((row: any, rowIndex: number) => (
+            <Grid item xs={12} key={rowIndex}>
+              <Grid container justifyContent="center">
+                {row.map((cell: any, colIndex: number) => {
+                  if (colIndex === 0 && rowIndex === 0) {
+                    return (
+                      <div
+                        key={colIndex}
+                        style={{
+                          border: "1px solid grey",
+                          color: "black",
+                          width: "57px",
+                        }}
+                      >
+                        {" "}
+                      </div>
+                    );
+                  } else if (colIndex === 0) {
+                    return (
+                      <Button
+                        key={colIndex}
+                        variant="outlined"
+                        sx={{ border: "1px solid grey", color: "black" }}
+                        onClick={() => handleChange(cell.id, rowIndex)} // Note: Adjusted for any errors in typing
+                      >
+                        {cell ? cell.id : ""}{" "}
+                        {/* Display the text property if the cell is not null */}
+                      </Button>
+                    );
+                  } else if (rowIndex === 0) {
+                    return (
+                      <Button
+                        key={colIndex}
+                        variant="outlined"
+                        sx={{ border: "1px solid grey", color: "black" }}
+                        onClick={() => handleChange(cell.id, colIndex)}
+                      >
+                        {cell ? cell.id : ""}{" "}
+                        {/* Display the text property if the cell is not null */}
+                      </Button>
+                    );
+                  } else {
+                    return (
+                      <Grid item key={colIndex}>
+                        <Cell
+                          rowIndex={rowIndex}
+                          colIndex={colIndex}
+                          cell={cell}
+                          moveBlock={moveBlock}
+                        />
+                      </Grid>
+                    );
+                  }
+                })}
+              </Grid>
+            </Grid>
+          ))}
+       </div>
+    )
+}
+
+export default GridComp;
